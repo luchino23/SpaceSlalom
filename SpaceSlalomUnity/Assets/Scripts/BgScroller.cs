0bad2407fd3f0acc4e752d3d038b793ec5f59b26
Giammarco Lucchetti <giammarcolucchetti1993@gmail.com>
2019-05-10 12:15:12 +0200

    ok

---
+++ SpaceSlalomUnity/Assets/Scripts/BgScroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BgScroller : MonoBehaviour
+{
+    public float speed = 0.1f;
+    float x;
+    Vector2 offSet;
+    Renderer mat;
+    
+    void Awake()
+    {
+        mat = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Mathf.Repeat(Time.time * speed, 1);
+        offSet = new Vector2(x, 0f);
+        mat.sharedMaterial.SetTextureOffset("_MainTex", offSet);
+    }
+}
---
+++ SpaceSlalomUnity/Assets/Scripts/StartGame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    public Canvas startGame;
+    float start = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        start = start + Time.deltaTime;
+        if (start > 2.3f)
+        {
+            //startGame.GetComponent<Canvas>().enabled = false;
+            startGame.enabled = false;
+        }
+    }
+}
