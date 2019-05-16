fc24a88b0c165880cfe1ca13767582dd922bd99e
Giammarco Lucchetti <giammarcolucchetti1993@gmail.com>
2019-05-16 15:28:15 +0200

    add prefab and collider

---
+++ SpaceSlalomUnity/Assets/Packet.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class Packet : MonoBehaviour
+{
+    private MemoryStream stream;
+    private BinaryWriter writer;
+
+    private static uint packetCounter;
+
+    private uint id;
+    public uint Id
+    {
+        get
+        {
+            return id;
+        }
+    }
+
+    public Packet()
+    {
+        stream = new MemoryStream();
+        writer = new BinaryWriter(stream);
+        id = ++packetCounter;
+    }
+
+    public Packet(byte commandNumber, params object[] elements) : this()
+    {
+        writer.Write(commandNumber);
+        foreach (object element in elements)
+        {
+            if (element is int)
+            {
+                writer.Write((int)element);
+            }
+            else if (element is float)
+            {
+                writer.Write((float)element);
+            }
+            else if (element is byte)
+            {
+                writer.Write((byte)element);
+            }
+            else if (element is char)
+            {
+                writer.Write((char)element);
+            }
+            else if (element is uint)
+            {
+                writer.Write((uint)element);
+            }
+            else
+            {
+                throw new Exception("unknown type");
+            }
+        }
+    }
+
+    public byte[] GetData()
+    {
+        return stream.ToArray();
+    }
+}
---
+++ SpaceSlalomUnity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        GetComponent<Rigidbody2D>().velocity = movement * speed;
+
+        Debug.Log("posizione player : " + transform.position);
+    }
+
+}
